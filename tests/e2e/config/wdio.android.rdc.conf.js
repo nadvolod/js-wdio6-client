f4862b95cbfee5b24075f576feccb6a21d02b9bb
nadvolod <nadvolod@gmail.com>
2020-05-06 11:43:29 -0400

    remove duplication

--- tests/e2e/config/wdio.android.rdc.conf.js
+++ tests/e2e/config/wdio.android.rdc.conf.js
@@ -6,39 +6,32 @@ const { config } = require('./wdio.rdc.shared');
 // ============
 // For all capabilities please check
 // http://appium.io/docs/en/writing-running-appium/caps/#general-capabilities
-commonCapabilities: [{
-  automationName: 'UiAutomator2', //automationName should always be provided for android,
-}],
+const commonCapabilities = {
+  //automationName should always be provided for android
+  automationName: 'UiAutomator2',
+  // The api key that has a reference to the app-project in the TO cloud
+  testobject_api_key: process.env.SAUCE_RDC_US_HILTON_ANDROID,
+  platformName: 'Android',
+};
 
 config.capabilities = [
   {
-    //samsung galaxy note
+    ...commonCapabilities,
     deviceName: 'Samsung Galaxy S9',
-    automationName: 'UiAutomator2', //automationName should always be provided for android
-    // The api key that has a reference to the app-project in the TO cloud
-    testobject_api_key: process.env.SAUCE_RDC_US_HILTON_ANDROID,
     // The name of the test for in the cloud
     testobject_test_name: 'Samsung Galaxy S9',
     // Some default settings
     // You can find more info in the TO Appium Basic Setup section
-    //appWaitActivity: 'com.swaglabsmobileapp.MainActivity',
-    platformName: 'Android',
     phoneOnly: 'true',
     idleTimeout: 180,
-    //noReset: true,
     orientation: 'PORTRAIT',
     newCommandTimeout: 30,
-    // language: argv.language || 'en',
-    // locale: argv.language || 'en',
-  },
+},
   {
+    ...commonCapabilities,
     deviceName: 'Samsung Galaxy Note 8',
-    automationName: 'UiAutomator2', //automationName should always be provided for android
-    // The api key that has a reference to the app-project in the TO cloud
-    testobject_api_key: process.env.SAUCE_RDC_US_HILTON_ANDROID,
     // The name of the test for in the cloud
     testobject_test_name: 'Google Pixel 3',
-    platformName: 'Android',
     phoneOnly: 'true',
     idleTimeout: 180,
     orientation: 'PORTRAIT',
